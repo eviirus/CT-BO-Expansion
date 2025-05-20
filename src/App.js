9d7b7410cf9922f8c4d866a59998d6d2218c02b7
eviirus <evaldas.zuralovic@gmail.com>
2025-05-20 17:13:24 +0300

    created new route + applied layout

--- src/App.js
+++ src/App.js
@@ -3,13 +3,30 @@ import { Routes, Route } from "react-router-dom";
 import React, { lazy, Suspense } from "react";
 
 import Homepage from "./routes/Homepage";
+import AppLayout from "./layouts/AppLayout";
+
+const ExcursionPageGenerator = lazy(() =>
+  import("./routes/landing-pages/excursions/ExcursionPageGenerator")
+);
 
 function App() {
   return (
     <HelmetProvider>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route path="/" element={<Homepage />} />
+            <Route path="landing-pages">
+              <Route path="excursions">
+                <Route
+                  path="excursion-page-generator"
+                  element={<ExcursionPageGenerator />}
+                />
+              </Route>
+            </Route>
+          </Route>
+        </Routes>
+      </Suspense>
     </HelmetProvider>
   );
 }
