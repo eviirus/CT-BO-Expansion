9114e36707e195989efd03837678f52570568d3b
eviirus <evaldas.zuralovic@gmail.com>
2025-05-20 17:13:11 +0300

    small edit for homepage

--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,27 +1,16 @@
 import { Helmet } from "react-helmet-async";
-import SideBar from "../components/navigation/side-bar/SideBar";
-import { Layout } from "antd";
 
-const { Content } = Layout;
+export default function Homepage() {
+  const pageTitle = "CO BO Expansion";
 
-export default function Homepage(){
-    const pageTitle = "CO BO Expansion"
-
-    return(
-        <>
-            <Helmet>
-                <title>{pageTitle}</title>
-                <meta name="title" content={pageTitle}/>
-            </Helmet>
-            <Layout style={{ minHeight: "100vh" }}>
-            <SideBar />
-            <Layout>
-                <Content style={{padding: "16px", backgroundColor: "#fff"}}>
-                    <h1 className="regular24">Welcome to CT BO Expansion</h1>
-                    <p className="regular14">Use the side menu to select a tool</p>
-                </Content>
-            </Layout>
-        </Layout>
-        </>
-    )
+  return (
+    <>
+      <Helmet>
+        <title>{pageTitle}</title>
+        <meta name="title" content={pageTitle} />
+      </Helmet>
+      <h1 className="regular24">Welcome to CT BO Expansion</h1>
+      <p className="regular14">Use the side menu to select a tool</p>
+    </>
+  );
 }
