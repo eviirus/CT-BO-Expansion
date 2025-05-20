e2fc4627f4c306280f45265fb9112d3bf14132dc
eviirus <evaldas.zuralovic@gmail.com>
2025-05-20 17:20:15 +0300

    added footer

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -2,16 +2,19 @@ import { Layout } from "antd";
 import SideBar from "../components/navigation/side-bar/SideBar";
 import { Outlet } from "react-router-dom";
 
-const { Content } = Layout;
+const { Content, Footer } = Layout;
 
 export default function AppLayout() {
     return (
         <Layout style={{ minHeight: "100vh" }}>
             <SideBar />
             <Layout>
-                <Content style={{ padding: "16px", background: "#f5f5f5" }}>
+                <Content style={{ padding: "16px", backgroundColor: "#f5f5f5" }}>
                     <Outlet />
                 </Content>
+                <Footer style={{backgroundColor: "#fff"}}>
+                    ©2025
+                </Footer>
             </Layout>
         </Layout>
     );
