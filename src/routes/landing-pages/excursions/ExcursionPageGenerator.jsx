45a3a230d6e6594c6faf202206b58abc6fdf9efa
eviirus <evaldas.zuralovic@gmail.com>
2025-05-22 14:23:32 +0300

    added hero content

--- src/routes/landing-pages/excursions/ExcursionPageGenerator.jsx
+++ src/routes/landing-pages/excursions/ExcursionPageGenerator.jsx
@@ -1,5 +1,25 @@
-export default function ExcursionPageGenerator(){
-    return(
-        <h1>excursions</h1>
-    )
+import { useRef } from "react";
+import HeroContent from "../../../components/landing-pages/hero-content/HeroContent";
+import BodyContent from "../../../components/landing-pages/body-content/BodyContent";
+import { toast } from "react-toastify";
+import { generateErrorMessage } from "../../../constants/alertMessages";
+
+export default function ExcursionPageGenerator() {
+  const heroRef = useRef();
+
+  const handleSubmit = () => {
+    const isHeroValid = heroRef.current?.validate?.();
+
+    if (!isHeroValid) {
+      toast.error(generateErrorMessage);
+    }
+  };
+  return (
+    <main>
+      <h1 className="regular28">Excursion page generator</h1>
+      <HeroContent ref={heroRef} pageTitle={true} imageLink={true} />
+      <BodyContent />
+      <button onClick={handleSubmit}>Submit</button>
+    </main>
+  );
 }
