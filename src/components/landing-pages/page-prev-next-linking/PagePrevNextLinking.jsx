3016b6a2442d8e2f5cccb7f63e795c8ea65cc899
eviirus <evaldas.zuralovic@gmail.com>
2025-05-24 12:51:35 +0300

    added linking with validation

---
+++ src/components/landing-pages/page-prev-next-linking/PagePrevNextLinking.jsx
@@ -0,0 +1,125 @@
+import { Input } from "antd";
+import { forwardRef, useImperativeHandle, useState } from "react";
+
+const PagePrevNextLinking = forwardRef((props, ref) => {
+  const [errors, setErrors] = useState({});
+  const [values, setValues] = useState({
+    prevPageTitle: "",
+    prevPageLink: "",
+    nextPageTitle: "",
+    nextPageLink: "",
+  });
+
+  const handleChange = (key, value) => {
+    setValues((prev) => {
+      const newValues = { ...prev, [key]: value };
+
+      if (
+        (key === "prevPageTitle" || key === "nextPageTitle") &&
+        value === ""
+      ) {
+        if (key === "prevPageTitle") {
+          newValues.prevPageLink = "";
+        } else if (key === "nextPageTitle") {
+          newValues.nextPageLink = "";
+        }
+      }
+
+      return newValues;
+    });
+  };
+
+  useImperativeHandle(ref, () => ({
+    validate: () => {
+      const newErrors = {};
+      const urlRegex = /^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/gm;
+
+      if (values.prevPageTitle.trim() !== "") {
+        if (values.prevPageLink.trim() === "") {
+          newErrors.prevPageLink =
+            "Previous page link is required. Add it or remove previous page title";
+        } else if (values.prevPageLink.trim() !== "") {
+          if (!urlRegex.test(values.prevPageLink)) {
+            newErrors.prevPageLink = "Please enter a valid URL";
+          }
+        }
+      }
+
+      if (values.nextPageTitle.trim() !== "") {
+        if (values.nextPageLink.trim() === "") {
+          newErrors.nextPageLink =
+            "Next page link is required. Add it or remove next page title";
+        } else if (values.nextPageLink.trim() !== "") {
+          if (!urlRegex.test(values.nextPageLink)) {
+            newErrors.nextPageLink = "Please enter a valid URL";
+          }
+        }
+      }
+
+      setErrors(newErrors);
+      return Object.keys(newErrors).length === 0;
+    },
+  }));
+
+  return (
+    <section className="page-prev-next-linking basic-col">
+      <h2 className="regular24">Linking</h2>
+      <div className="basic-row">
+        <div className="basic-col">
+          <div className="basic-col">
+            <label htmlFor="prevPageTitle">Previous page title</label>
+            <Input
+              value={values.prevPageTitle}
+              onChange={(value) =>
+                handleChange("prevPageTitle", value.target.value)
+              }
+              allowClear
+            />
+          </div>
+          <div className="basic-col">
+            <label htmlFor="prevPageLink">Previous page link</label>
+            <Input
+              value={values.prevPageLink}
+              onChange={(value) =>
+                handleChange("prevPageLink", value.target.value)
+              }
+              disabled={!values.prevPageTitle.trim()}
+              allowClear
+            />
+            {errors.prevPageLink && (
+              <div className="error">{errors.prevPageLink}</div>
+            )}
+          </div>
+        </div>
+        <div className="basic-col">
+          <div className="basic-col">
+            <label htmlFor="nextPageTitle">Next page title</label>
+            <Input
+              value={values.nextPageTitle}
+              onChange={(value) =>
+                handleChange("nextPageTitle", value.target.value)
+              }
+              allowClear
+            />
+          </div>
+          <div className="basic-col">
+            <label htmlFor="nextPageLink">Next page link</label>
+            <Input
+              value={values.nextPageLink}
+              onChange={(value) =>
+                handleChange("nextPageLink", value.target.value)
+              }
+              disabled={!values.nextPageTitle.trim()}
+              allowClear
+            />
+            {errors.nextPageLink && (
+              <div className="error">{errors.nextPageLink}</div>
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+});
+
+export default PagePrevNextLinking;
--- src/routes/landing-pages/excursions/ExcursionPageGenerator.jsx
+++ src/routes/landing-pages/excursions/ExcursionPageGenerator.jsx
@@ -1,12 +1,14 @@
 import { useRef } from "react";
 import HeroContent from "../../../components/landing-pages/hero-content/HeroContent";
 import BodyContent from "../../../components/landing-pages/body-content/BodyContent";
+import PagePrevNextLinking from "../../../components/landing-pages/page-prev-next-linking/PagePrevNextLinking";
 import { toast } from "react-toastify";
 import { generateErrorMessage } from "../../../constants/alertMessages";
 
 export default function ExcursionPageGenerator() {
   const heroRef = useRef();
   const bodyContentRef = useRef();
+  const pagePrevNextLinkingRef = useRef();
 
   const content = [
     {
@@ -55,9 +57,13 @@ export default function ExcursionPageGenerator() {
 
   const handleSubmit = () => {
     const isHeroValid = heroRef.current?.validate?.();
-    const bodyValidation = bodyContentRef.current?.validate?.();
+    const isBodyValid = bodyContentRef.current?.validate?.();
+    const isPagePrevNextLinkingValid =
+      pagePrevNextLinkingRef.current?.validate?.();
 
-    if (!isHeroValid || !bodyValidation) {
+    console.log(isHeroValid, isBodyValid, isPagePrevNextLinkingValid);
+
+    if (!isHeroValid || !isBodyValid || !isPagePrevNextLinkingValid) {
       toast.error(generateErrorMessage);
       return;
     }
@@ -69,6 +75,7 @@ export default function ExcursionPageGenerator() {
       <h1 className="regular28">Excursion page generator</h1>
       <HeroContent ref={heroRef} pageTitle={true} imageLink={true} />
       <BodyContent ref={bodyContentRef} content={content} />
+      <PagePrevNextLinking ref={pagePrevNextLinkingRef} />
       <button onClick={handleSubmit}>Submit</button>
     </>
   );
