afe27bf6a4f91910b85b5c303ad2297a2d09c9c6
eviirus <evaldas.zuralovic@gmail.com>
2025-06-19 11:30:50 +0300

    fixed warning

--- src/hooks/useTypeChange.jsx
+++ src/hooks/useTypeChange.jsx
@@ -6,24 +6,27 @@ export const useTypeChange = ({
   setErrors,
   setClasses,
 }) => {
-  const handleTypeChange = useCallback((key, typeValue) => {
-    setSelectedType((prev) => ({ ...prev, [key]: typeValue }));
-    setErrors((prev) => ({
-      ...prev,
-      type: {
-        ...(prev.type || {}),
-        [key]: "",
-      },
-    }));
-
-    const rowClasses = content
-      .filter((item) => item.type === typeValue && item.class !== "")
-      .map((item) => ({
-        label: item.class,
-        value: item.class,
+  const handleTypeChange = useCallback(
+    (key, typeValue) => {
+      setSelectedType((prev) => ({ ...prev, [key]: typeValue }));
+      setErrors((prev) => ({
+        ...prev,
+        type: {
+          ...(prev.type || {}),
+          [key]: "",
+        },
       }));
 
-    setClasses((prev) => ({ ...prev, [key]: rowClasses }));
-  });
+      const rowClasses = content
+        .filter((item) => item.type === typeValue && item.class !== "")
+        .map((item) => ({
+          label: item.class,
+          value: item.class,
+        }));
+
+      setClasses((prev) => ({ ...prev, [key]: rowClasses }));
+    },
+    [content, setSelectedType, setErrors, setClasses]
+  );
   return { handleTypeChange };
 };
